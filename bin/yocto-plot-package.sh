82d85f22249a5bb414c34d5ee90edfa957b4ff32
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-10-24 12:59:05 +0200

    first rev

---
+++ bin/yocto-plot-package.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# default
+DOT_FILE_NAME="depends.dot"
+TMP_DIR=~/.vinland-compliance-utils/plot-package
+
+usage()
+{
+    echo "usage text coming soon"
+}
+
+while [ "$1" != "" ]
+do
+    case "$1" in
+        "--depends-file" | "-df")
+            DOT_FILE_NAME="$2"
+            shift
+            ;;
+        "--tmp-dir" | "-td")
+            TMP_DIR="$2"
+            shift
+            ;;
+        "--help" | "-h")
+            usage
+            exit 0
+            ;;
+        *)
+            # assume package
+            PKG="$1"
+    esac
+    shift
+done
+
+DOT_FILE="$(dirname $(realpath $0))/resources/$DOT_FILE_NAME"
+PKG_DOT_FILE="$TMP_DIR/${PKG}.dot"
+mkdir -p $TMP_DIR
+
+if [ "$PKG" = "" ]
+then
+    echo "No package specified"
+    exit 1
+fi
+
+if [ "$DOT_FILE_NAME" = "" ] | [ ! -f $DOT_FILE_NAME ] 
+then
+    echo "No depends.dot found (\"$DOT_FILE_NAME\")"
+    exit 1
+fi
+
+create_dot()
+{
+    head -2 "$DOT_FILE"
+    grep "$PKG" "$DOT_FILE"
+    tail -1 "$DOT_FILE"
+}
+
+create_pdf()
+{
+    dot -Tpdf -O "$PKG_DOT_FILE"
+}
+
+create_dot > $PKG_DOT_FILE
+
+create_pdf
+echo "Created $PKG_DOT_FILE.pdf"
+
