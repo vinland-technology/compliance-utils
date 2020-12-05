92240367c76e8fc6efad1ba4f97b273241dd17f6
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-12-05 18:07:54 +0100

    first rev, setup sctips

---
+++ var/setup-linux-fedora.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PACKAGES="python3 elfutils jq graphviz zip "
+
+sudo yum install $PACKAGES 
+
---
+++ var/setup-linux-ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PACKAGES="python3 elfutils jq graphviz zip "
+
+sudo apt-get install $PACKAGES 
+
